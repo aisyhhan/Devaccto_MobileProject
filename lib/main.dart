0a91b425f5d3a46fe389babac92d4447d031518f
Fathan Syarifullah <syarifullahfathan@gmail.com>
2026-01-22 09:06:01 +0700

    nyoba branch

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class DevacctoProject extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Devaccto flutter',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
